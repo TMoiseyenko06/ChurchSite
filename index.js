9300739dff5f20db740d69eeace8affbba72ec2d
TMoiseyenko06 <140467502+TMoiseyenko06@users.noreply.github.com>
2023-11-11 14:18:59 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+    // Get the `<div>` element
+    var div = document.querySelector("#div3");
+
+    // Add an event listener for the `mouseover` event
+    div.addEventListener("mouseover", function() {
+    // Expand the `<div>` element
+    this.style.width = "350px";
+    });
+
+    // Add an event listener for the `mouseout` event
+    div.addEventListener("mouseout", function() {
+    // Shrink the `<div>` element back to its original size
+    this.style.width = "300px";
+    });
+
+    // Get the `<div>` element
+    var div = document.querySelector("#div1");
+
+    // Add an event listener for the `mouseover` event
+    div.addEventListener("mouseover", function() {
+    // Expand the `<div>` element
+    this.style.width = "350px";
+    });
+
+    // Add an event listener for the `mouseout` event
+    div.addEventListener("mouseout", function() {
+    // Shrink the `<div>` element back to its original size
+    this.style.width = "300px";
+    });
+
+    // Get the `<div>` element
+    var div = document.querySelector("#div2");
+
+    // Add an event listener for the `mouseover` event
+    div.addEventListener("mouseover", function() {
+    // Expand the `<div>` element
+    this.style.width = "350px";
+    });
+
+    // Add an event listener for the `mouseout` event
+    div.addEventListener("mouseout", function() {
+    // Shrink the `<div>` element back to its original size
+    this.style.width = "300px";
+    });
