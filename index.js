994b4c73a670e2bbb9367af2073c656587efc8d4
TMoiseyenko06 <140467502+TMoiseyenko06@users.noreply.github.com>
2023-11-20 11:51:20 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,73 @@
-    // Get the `<div>` element
-    var div = document.querySelector("#div3");
 
-    // Add an event listener for the `mouseover` event
-    div.addEventListener("mouseover", function() {
-    // Expand the `<div>` element
+var div = document.querySelector("#div3");
+
+
+div.addEventListener("mouseover", function () {
+
     this.style.width = "350px";
-    });
+});
 
-    // Add an event listener for the `mouseout` event
-    div.addEventListener("mouseout", function() {
-    // Shrink the `<div>` element back to its original size
+
+div.addEventListener("mouseout", function () {
+
     this.style.width = "300px";
-    });
+});
 
-    // Get the `<div>` element
-    var div = document.querySelector("#div1");
 
-    // Add an event listener for the `mouseover` event
-    div.addEventListener("mouseover", function() {
-    // Expand the `<div>` element
+var div = document.querySelector("#div1");
+
+
+div.addEventListener("mouseover", function () {
+
     this.style.width = "350px";
-    });
+});
 
-    // Add an event listener for the `mouseout` event
-    div.addEventListener("mouseout", function() {
-    // Shrink the `<div>` element back to its original size
+
+div.addEventListener("mouseout", function () {
+
     this.style.width = "300px";
-    });
+});
 
-    // Get the `<div>` element
-    var div = document.querySelector("#div2");
 
-    // Add an event listener for the `mouseover` event
-    div.addEventListener("mouseover", function() {
-    // Expand the `<div>` element
+var div = document.querySelector("#div2");
+
+
+div.addEventListener("mouseover", function () {
+
     this.style.width = "350px";
-    });
+});
 
-    // Add an event listener for the `mouseout` event
-    div.addEventListener("mouseout", function() {
-    // Shrink the `<div>` element back to its original size
+
+div.addEventListener("mouseout", function () {
+
     this.style.width = "300px";
-    });
+});
+
+
+
+var slidePosition = 0;
+SlideShow();
+
+function SlideShow() {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidePosition++;
+  if (slidePosition > slides.length) {slidePosition = 1}
+  slides[slidePosition-1].style.display = "block";
+  setTimeout(SlideShow, 2000); }
+
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCPHrq5PCfCOE2wVTTNFakag&maxResults=10&order=date&key=AIzaSyBRHG5LtuK_OD0JxmajOOGqT8GSmJNsqBI")
+.then((result)=>{
+    return result.json()
+}).then((data)=>{
+    let videos = data.items
+    let videoContainer = document.querySelector(".youtube-container")
+    for(video of videos){
+        videoContainer.innerHTML += `
+            <img src="${video.snippet.thumbnails.default.url}">
+        `
+    }
+})
