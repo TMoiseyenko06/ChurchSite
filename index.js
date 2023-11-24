ab290caa409fe54343e6c0e0879cad9d2b41440a
TMoiseyenko06 <140467502+TMoiseyenko06@users.noreply.github.com>
2023-11-23 22:23:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCPHrq5PCfCOE2wVTTNFakag&maxResults=10&order=date&key=AIzaSyBRHG5LtuK_OD0JxmajOOGqT8GSmJNsqBI")
-.then((result)=>{
-    return result.json()
-}).then((data)=>{
-    let videos = data.items
-    let videoContainer = document.querySelector(".youtube-container")
-    for(video of videos){
-        videoContainer.innerHTML += `
-            <img src="${video.snippet.thumbnails.default.url}">
-        `
-    }
-})
+// ...
+
+function loadYouTubeAPI() {
+   gapi.client.init({
+       apiKey: 'AIzaSyBRHG5LtuK_OD0JxmajOOGqT8GSmJNsqBI',
+   }).then(function () {
+       return gapi.client.load('youtube', 'v3');
+   }).then(function () {
+       // Call the function to get the latest videos
+       getLatestVideos();
+   }).catch(function (error) {
+       console.error('Error loading YouTube API:', error);
+   });
+}
+
+function getLatestVideos() {
+   // Set the API request parameters
+   const request = gapi.client.youtube.search.list({
+       part: 'snippet',
+       channelId: 'UCPHrq5PCfCOE2wVTTNFakag',
+       order: 'date',
+       maxResults: 5, // You can adjust the number of videos to retrieve
+   });
+
+   // Execute the API request
+   request.execute(function (response) {
+       if (response.error) {
+           console.error('Error fetching videos:', response.error);
+           return;
+       }
+
+       const videosContainer = document.getElementById('videos-container');
+
+       // Iterate through the response items and create iframe elements
+       for (const item of response.result.items) {
+           const videoId = item.id.videoId;
+           const iframe = document.createElement('iframe');
+           iframe.src = `https://www.youtube.com/embed/${videoId}`;
+           iframe.width = 560; // You can adjust the width
+           iframe.height = 315; // You can adjust the height
+           iframe.allowFullscreen = true;
+
+           // Append the iframe to the container
+           videosContainer.appendChild(iframe);
+       }
+   });
+}
+
+// ...
