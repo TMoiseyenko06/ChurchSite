b0ebc7e360508b5912b353e08f54a16099eb1cca
TMoiseyenko06 <140467502+TMoiseyenko06@users.noreply.github.com>
2023-12-02 15:27:05 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
-        // Replace 'YOUR_API_KEY' with your actual API key
-        const apiKey = 'AIzaSyBRHG5LtuK_OD0JxmajOOGqT8GSmJNsqBI';
-        const channelId = 'UCPHrq5PCfCOE2wVTTNFakag'; // Replace with the target YouTube channel ID
 
-        function loadYouTubeAPI() {
-            gapi.client.init({
-                'apiKey': apiKey,
-            }).then(function () {
-                return gapi.client.load('youtube', 'v3');
-            }).then(function () {
-                // Call the function to get the latest videos
-                getLatestVideos();
-            });
+const apiKey = 'AIzaSyBRHG5LtuK_OD0JxmajOOGqT8GSmJNsqBI';
+const channelId = 'UCPHrq5PCfCOE2wVTTNFakag';
+const maxResults = 4; 
+
+function embedLatestVideos(videos) {
+    const videosContainer = document.getElementById('videos-container');
+
+    videos.forEach(video => {
+        const iframe = document.createElement('iframe');
+        iframe.src = `https://www.youtube.com/embed/${video.id.videoId}`;
+        iframe.width = 320; 
+        iframe.height = 180; 
+        iframe.allowFullscreen = true;
+
+        videosContainer.appendChild(iframe);
+    });
+}
+
+async function getLatestVideos() {
+    const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&order=date&maxResults=${maxResults}&key=${apiKey}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (data.items) {
+            embedLatestVideos(data.items);
+        } else {
+            console.error('Error fetching videos:', data.error.message);
         }
+    } catch (error) {
+        console.error('Error fetching videos:', error);
+    }
+}
 
-        function getLatestVideos() {
-            // Set the API request parameters
-            const request = gapi.client.youtube.search.list({
-                part: 'snippet',
-                channelId: channelId,
-                order: 'date',
-                maxResults: 5, // You can adjust the number of videos to retrieve
-            });
 
-            // Execute the API request
-            request.execute(function (response) {
-                const videosContainer = document.getElementById('videos-container');
-
-                // Iterate through the response items and create iframe elements
-                for (const item of response.result.items) {
-                    const videoId = item.id.videoId;
-                    const iframe = document.createElement('iframe');
-                    iframe.src = `https://www.youtube.com/embed/${videoId}`;
-                    iframe.width = 560; // You can adjust the width
-                    iframe.height = 315; // You can adjust the height
-                    iframe.allowFullscreen = true;
-
-                    // Append the iframe to the container
-                    videosContainer.appendChild(iframe);
-                }
-            });
-        }
-
-        // Load the YouTube API asynchronously
-        gapi.load('client', loadYouTubeAPI);
+getLatestVideos();
